92d9930ba6ec52ce11a6737bb4659feb3d2d77ba
henrylin <46950977+linhungyu@users.noreply.github.com>
2022-04-20 23:40:47 +0800

    [FIX] develop predict function

--- LSTM.py
+++ LSTM.py
@@ -6,6 +6,16 @@ import torch.optim as optim
 def tensorized(data):
     return torch.tensor(data)
     
+def post_process(input_x):
+
+    Batch, seq, hidden = input_x.size()
+    
+    indices = torch.tensor([seq-1])
+    temp = torch.index_select(input_x, 1, indices)
+    # print("temp.size(): ", temp.size())
+    # print(input_x[240,49,:])
+    # print(temp[240,:])
+    return temp.squeeze(1)
 
 
 
@@ -16,17 +26,17 @@ class LSTM(nn.Module):
         
         self.h0 = torch.randn(2, 3, 20)
         self.c0 = torch.randn(2, 3, 20)
-        input_feat_dim=1
-        hidden_feat_dim=32
-        hidden_layer_dim=1
-        self.model = nn.LSTM(input_feat_dim, hidden_feat_dim, batch_first=True) 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)   
+        input_feat_dim=4
+        hidden_feat_dim=3
+        hidden_layer_dim=3
+        self.model = nn.LSTM(input_feat_dim, hidden_feat_dim, hidden_layer_dim, batch_first=True)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.01)   
         self.loss_fnc = nn.CrossEntropyLoss()
     
     
     def forward(self, input_data):
         rnn_out, (h_n, c_n) = self.model(input_data)
-        print(rnn_out.size())
+        # print(rnn_out.size())
         return rnn_out
 
 
@@ -40,8 +50,12 @@ class LSTM(nn.Module):
             epoch_loss = 0
             for i in range(1, 2):
                 self.model.zero_grad()
-                
+                # print("x_train.shape: ", x_train.shape)
                 output = self.forward(x_train)
+                output = post_process(output)
+                # exit()
+                # print("output.shape: ", output.shape)
+                # print("y_train.shape: ", y_train.shape)
                 
                 loss = self.loss_fnc(output,y_train)
                 epoch_loss += loss
@@ -49,3 +63,22 @@ class LSTM(nn.Module):
                 self.optimizer.step()
                 
             print(f" Epoch {epoch} | loss: {epoch_loss}")
+
+
+    def predict(self, x_test):
+        self.model.eval()
+        x_test = tensorized(x_test).to(torch.float32).unsqueeze(0)
+
+        print("x_test.shape: ", x_test.shape)
+        output = self.forward(x_test)
+        output = post_process(output)
+        print("output.shape: ", output.shape)
+        print("output: ", output)
+        index = torch.argmax(output, dim=1)
+        print("index.shape: ", index.shape)
+
+
+
+
+
+
--- main_lstm.py
+++ main_lstm.py
@@ -3,6 +3,27 @@ import pandas as pd
 from LSTM import LSTM
 from sklearn.preprocessing import StandardScaler
 
+
+def load_g_truth(file_path):
+    with open(file_path, 'r', encoding='utf8') as f:
+        temp = f.read().splitlines()
+    
+    temp = list(map(int, temp))
+
+    result = []
+    for value in temp:
+        if value == 1:
+            result.append([1, 0, 0])
+        elif value == 0:
+            result.append([0, 1, 0])
+        else:
+            result.append([0, 0, 1])
+
+    
+
+    return result
+
+
 def load_data(file_name):
     df = pd.read_csv(file_name)
     df.columns = ['open', 'high', 'low', 'close']
@@ -12,26 +33,31 @@ def load_data(file_name):
 
 
 def data_trans(df) -> list:
+    '''
+    Transform dataframe into 2D array for StandardScaler()
+    '''
     result = []
-    col = df.iloc[:,3]
-    for value in col:
-        result.append([value])
+    # col = df.iloc[:,3]
+    # for value in col:
+        # result.append([value])
+    for index, row in df.iterrows():
+        result.append([row['open'], row["high"], row["low"], row["close"]])
 
     return result
 
 ## Split the time-series data into training seq X and output value Y
-def extract_seqX_outcomeY(data, window_size=50, offset=5):
+def extract_seqX_outcomeY(data, window_size=50, offset=50):
     """
     Split time-series into training sequence X and outcome value Y
     Args:
-        data : dataset 
+        data : (Dataframe) dataset 
         window_size : window size, e.g., 50 for 50 days of historical stock prices
         offset : position to start the split
     """
     X, y = [], []
 
     for i in range(offset, len(data)):
-        X.append( data[i-offset : i] )
+        X.append( data[i-window_size : i] )
         y.append(data[i])
 
     return np.array(X), np.array(y)
@@ -40,16 +66,30 @@ def extract_seqX_outcomeY(data, window_size=50, offset=5):
 def main():
     
     train_data = load_data("training.csv")
-    data_close = data_trans(train_data)
+    train_data = data_trans(train_data)
     scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(data_close)
+    # train_data = scaler.fit_transform(train_data)
     # print(scaled_data)
     
-    X_train, y_train = extract_seqX_outcomeY(scaled_data, window_size=50, offset=10)
-    model = LSTM()
-    model.train(X_train, y_train)
+    x_train, _ = extract_seqX_outcomeY(train_data, window_size=50, offset=50)
 
-    
+    y_train = load_g_truth("output.txt")
+    y_train = y_train[50:]
+    model = LSTM()
+    model.train(x_train, y_train)
+
+    # ================ Prediction stage ================
+    temp = x_train[len(x_train)-1]
+    temp = temp[2:]
+    print(temp)
+    temp = np.append(temp, [[153.65, 154.41, 153.08, 153.97]], axis=0)
+    temp = np.append(temp, [[154.4, 155.02, 152.91, 154.76]],  axis=0)
+    print(len(temp))
+    # exit()
+    # x_test = x_train[-1,:,:]
+    model.predict(temp)
+    # print(len(x_test))
+
     
 
 
